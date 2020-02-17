25d02e591437cc36e266bba56e234bcb4898c0ad
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2020-02-17 14:00:33 -0500

    Update point class

--- point/point.hpp
+++ point/point.hpp
@@ -1,36 +1,54 @@
 /**
- * @file point.hpp
+ *  @file point.hpp
  *
- * Illustrate classes via a point class.
+ *  Defined point class to illustrate basic classes.
  *
- * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ *  @author Michael John Decker <mdecker6@kent.edu>
  */
 
-#ifndef INCLUDED_POINT_HPP
-#define INCLUDED_POINT_HPP
+#ifndef CS2_POINT_HPP
+#define CS2_POINT_HPP
 
 class point {
+  // class defaults to private
+  // struct default to public
+  // if has more than just data, make it a class
 private:
+    // data members
+    // private only members (constructors/methods)
+    // can access
+    // if public, anything can access
+    // make private, protects access
     int x;
     int y;
+
 public:
+    // constructors, generally public
     point();
     point(int x, int y);
 
+    // methods are generally public
+  
+    // getter methods
+    // always need const version, so const object
+    // can call method
+    // may need non const
     int get_x() const;
     int get_y() const;
 
-    void set_x(int new_x);
-    void set_y(int new_y);
+    // setter method
+    void set_x(int x);
+    void set_y(int y);
+
+    point add(const point & other);
+    point operator+(const point & other);
+    // friend point operator+(point p, const point & rhs);
+
+    int distance(const point & other);
+    int operator-(const point & rhs);
 
     void print() const;
 
-    point add(const point & other) const;
-    point operator+(const point & other) const;
-
-    int operator-(const point & other) const;
-
 };
-
 
 #endif
