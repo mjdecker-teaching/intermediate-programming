ea89d2b23c43073f71ac5a8e2af7ed81602abe47
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2020-03-30 12:02:17 -0400

    Add queue

---
+++ queue/main.cpp
@@ -0,0 +1,70 @@
+/**
+ *  @file main.cpp
+ *
+ *  This is to test stack class.
+ *
+ *  @author Michael John Decker <mdecker6@kent.edu>
+ */
+
+#include <iostream>
+#include <string>
+#include "queue.hpp"
+
+int main(int argc, char * argv[]) {
+
+    queue<int> int_queue;
+    int num = 1;
+    while(true) {
+        if(num == 4) break;
+        int_queue.enqueue(num++);
+
+        
+    }
+
+    while(!int_queue.empty()) {
+        std::cout << "front: " << int_queue.front() << '\n';
+        int_queue.dequeue();
+    }
+
+    while(true) {
+        if(num == 4) break;
+        int_queue.enqueue(num++);
+    }
+
+    while(!int_queue.empty()) {
+        std::cout << "front: " << int_queue.front() << '\n';
+        int_queue.dequeue();
+    }
+
+    std::cout << "\n\n";
+
+    queue<int> two;
+
+    two.enqueue(1);
+    std::cout << "front: " << two.front() << '\n';
+    two.enqueue(2);
+    std::cout << "front: " << two.front() << '\n';
+    two.dequeue();
+    std::cout << "front: " << two.front() << '\n';
+    two.enqueue(3);
+    std::cout << "front: " << two.front() << '\n';
+    two.enqueue(4);
+    std::cout << "front: " << two.front() << '\n';
+    two.dequeue();
+    std::cout << "front: " << two.front() << '\n';
+    two.enqueue(5);
+    std::cout << "front: " << two.front() << '\n';
+    two.enqueue(6);
+    std::cout << "front: " << two.front() << '\n';
+    std::cout << '\n';
+    two.dequeue();
+    std::cout << "front: " << two.front() << '\n';
+    two.dequeue();
+    std::cout << "front: " << two.front() << '\n';
+    two.dequeue();
+    std::cout << "front: " << two.front() << '\n';
+    two.dequeue();
+    std::cout << "empty: " << std::boolalpha << two.empty() << '\n';
+
+    return 0;
+}
---
+++ queue/queue.hpp
@@ -0,0 +1,65 @@
+#ifndef CS2_QUEUE_HPP
+#define CS2_QUEUE_HPP
+
+#include <cassert>
+
+const int MAX_SIZE = 4;
+
+template<typename type>
+class queue {
+private:
+    int start_pos;
+    int end_pos;
+    int size;
+
+    type buffer[MAX_SIZE];
+
+public:
+
+    queue() : start_pos(0), end_pos(-1), size(0) {}
+
+    void enqueue(const type & item) {
+	assert(!full());
+	++size;
+	++end_pos;
+	end_pos %= MAX_SIZE;
+	buffer[end_pos] = item;
+    }
+
+    void dequeue() {
+	assert(!empty());
+	--size;
+	++start_pos;
+	start_pos %= MAX_SIZE;
+    }
+    
+    type front() const {
+	assert(!empty());
+	return buffer[start_pos];
+    }
+
+    type & front() {
+	assert(!empty());
+	return buffer[start_pos];
+    }
+
+    type back() const {
+	assert(!empty());
+	return buffer[end_pos];
+    }
+
+    type & back() {
+	assert(!empty());
+	return buffer[end_pos];
+    }
+
+    bool full() const {
+	return size == MAX_SIZE;
+    }
+    bool empty() const {
+	return size == 0;
+    }
+
+};
+
+#endif
