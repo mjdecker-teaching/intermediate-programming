02556868a5547f4a28fe9f2dbf566b66fc899260
Michael J. Decker, Ph.D <mdecke@bgsu.edu>
2021-03-10 17:08:29 -0500

    Update main

--- array/main.cpp
+++ array/main.cpp
@@ -1,21 +1,32 @@
 /**
- * @file main.cpp
+ *  @file main.cpp
  *
- * This is a generic C++ main file.
+ *  This is a generic C++ main file.
  *
- * @author Michael John Decker, Ph.D. <mdecke@bgsu.edu>
+ *  @author Michael John Decker <mdecker6@kent.edu>
  */
 
 #include <iostream>
 #include "array.hpp"
 
-int main(int argc, char * argv[]) {
+int main() {
 
-    {
-	darray array(10);
+    darray<int> d(752);
 
-    } // array goes out scope and object goes way
-    //darray array_2 = array;
+    darray<int> copy(d);
+    darray<int> copy_two = d;
 
-    return 0;
+    copy = d;
+    d = d;
+
+    darray<int> d2(3);
+
+    d2.reset_capacity(7);
+    d2.reset_capacity(2);
+
+    std::cerr << d2 << '\n';
+
+    darray<double> e(752.0);
+
+    return 0; // destructor called for d copy
 }
